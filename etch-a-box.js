586d46f219fe904bce68bb672722650fcdbbda44
lschachter <lillieschachter@gmail.com>
2017-02-01 15:56:54 -0500

    Add rainbow option

--- etch-a-box.js
+++ etch-a-box.js
@@ -3,12 +3,13 @@ var grid_width = 600;
 $(document).ready(function() {
 	$('#container').css({'width':grid_width, 'height':grid_width});
 	start_up(10);
-	$('#reset').on('click',reset_grid);	
+	$('#reset').on('click',reset_grid);
+	$('#color-opts').on('change',set_color);
 });
 
 function start_up(n) {
 	pop_grid(n);
-	$('.etch-box').on('mouseenter',color_sqr);
+	$('.etch-box').on('mouseenter',{color: $('#color-opts').val()}, color_sqr);
 }
 
 function pop_grid(n){
@@ -25,12 +26,22 @@ function pop_grid(n){
 	}	
 }
 
-function color_sqr() {
-	$(this).addClass('etch-box-hover');
+function color_sqr(event) {
+	var choice = event.data.color;
+	if (choice ==="Rainbow") {
+		$(this).css({'background-color':'#'+(Math.random()*0xFFFFFF<<0).toString(16)});
+	} else {
+		$(this).css({'background-color': choice});
+	}
 }
 
 function reset_grid() {
 	var num_rows = prompt("How many squares per row would you like?");
 	$('#container').empty();
 	start_up(num_rows);
+}
+
+function set_color() {
+	var color = $('#color-opts').val();
+	$('.etch-box').on('mouseenter',{color: color}, color_sqr);
 }
